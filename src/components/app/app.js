44bd66d712e9c45c3142b33500899e96c5908978
isumanof <isumanof@gmail.com>
2020-08-23 15:19:21 +0300

    add: create PostList block and PostListItem block

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import AppHeader from '../app-header/';
+import SearchPanel from '../search-panel/';
+import PostStatusFilter from '../post-status-filter/';
+import PostList from '../post-list/';
+
 const App = () =>{
-    return (<h1>Hello</h1>)
+    return (
+       <div className="app">
+            <AppHeader />
+            <div className="search-panel d-flex">
+                <SearchPanel />
+                <PostStatusFilter />
+            </div>
+            <PostList />
+       </div>
+        
+    )
 }
 
 export default App;
