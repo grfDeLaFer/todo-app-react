8bd198d3b9758d2a9af998a5e9b2d282f5e9552c
isumanof <isumanof@gmail.com>
2020-08-27 21:10:46 +0300

    add: Search panel is done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,13 +18,15 @@ export default class App extends Component{
                 {label: "Прочитать книгу" , important: true, like: false, id: 1},
                 {label: "Отжаться 5 раз" , important: false , like: false, id: 2},
                 {label: "Сделать уборку" , important: false, like: false, id: 3}
-            ]
+            ],
+            term: ''
         }
 
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
         this.onToggleImportant = this.onToggleImportant.bind(this);
         this.onToggleLiked = this.onToggleLiked.bind(this);
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
 
         this.maxId = 4; 
     }
@@ -60,7 +62,18 @@ export default class App extends Component{
     }
 
     onToggleImportant(id){
-        console.log(`Important {id}`);
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id  === id);
+
+            const old = data[index];
+            const newItem = {...old, important: !old.important};
+
+            const newArr = [...data.slice(0,index),newItem, ...data.slice(index+1)];
+
+            return{
+                data: newArr
+            }
+        })
     }
 
     onToggleLiked(id){
@@ -78,10 +91,28 @@ export default class App extends Component{
         })
     }
 
+    searchPost(items, term){
+        if(term.length === 0){
+            return items; 
+        }
+
+        return items.filter( (item) => {
+            return item.label.indexOf(term) > -1;
+        })
+    }
+    
+    onUpdateSearch(term){
+        this.setState({term});
+    }
+
+
+
     render(){ 
-        const {data} = this.state;
+        const {data, term} = this.state;
         const liked = data.filter(item => item.like).length;
         const allPosts = data.length;
+
+        const visiblePosts = this.searchPost(data, term);
 
          return (
             <div className="app">
@@ -90,11 +121,11 @@ export default class App extends Component{
                         allPosts = {allPosts}
                     />
                     <div className="search-panel d-flex">
-                        <SearchPanel />
+                        <SearchPanel onUpdateSearch={this.onUpdateSearch} />
                         <PostStatusFilter />
                     </div>
                     <PostList
-                        posts={this.state.data}
+                        posts={visiblePosts}
                         onDelete={this.deleteItem}
                         onToggleImportant={this.onToggleImportant}
                         onToggleLiked={this.onToggleLiked}/>
--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React ,{Component} from 'react';
 
 import './post-add-form';
 
-const PostAddFrom = ({onAdd}) => {
-    return(
-        <div className="bottom-panel d-flex">
-            <input 
-                type="text"
-                placeholder="Напишите новое задание для Ярослава ..."
-                className="form-control new-post-label"
-            />
-            <button 
-                type="submit"
-                className="btn btn-outline-secondary"
-                onClick={() => onAdd('Hello')}>
-                Добавть
-            </button>
-        </div>
-    )
-};
 
-export default PostAddFrom;
+export default class PostAddFrom extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+             text: ''
+        }
+        this.onValueChange = this.onValueChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this); 
+    }
+    
+    onValueChange(e){
+        this.setState({
+            text : e.target.value })
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        this.props.onAdd(this.state.text)
+        this.setState({
+            text: ''
+        });
+    }
+
+    render(){
+        return(
+            <form className="bottom-panel d-flex"
+                  onSubmit={this.onSubmit}>
+                <input 
+                    type="text"
+                    placeholder="Напишите новое задание для Ярослава ..."
+                    className="form-control new-post-label"
+                    onChange={this.onValueChange}
+                    value={this.state.text}
+                />
+                <button 
+                    type="submit"
+                    className="btn btn-outline-secondary">
+                    Добавть
+                </button>
+            </form>
+        )
+    }
+}
